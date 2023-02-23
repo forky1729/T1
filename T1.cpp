1e8fef1b4c95f2c3d91e34cbe8dae1fff7e0eddf
Ulyana <113989749+forky1729@users.noreply.github.com>
2023-02-23 22:21:28 +0300

    Update T1.cpp

--- T1.cpp
+++ T1.cpp
@@ -29,7 +29,7 @@ namespace cat
         using size_type = std::size_t;
         using hasher = Hash;
         using key_equal = KeyEqual;
-        using iterator = &value_type;
+        using iterator = &value_type;                                                       //У
         using const_iterator = &const value_type;
         using local_iterator = iterator;
         using const_local_iterator = const_iterator;
@@ -45,7 +45,7 @@ namespace cat
         //using const_pointer = ???;
 
         //конструкторы =======================================================================
-        ~unordered_map();
+        ~unordered_map();                                                                   //А
         unordered_map();
         unordered_map( size_type bucket_count );
         unordered_map( size_type bucket_count,
@@ -66,31 +66,31 @@ namespace cat
         unordered_map( unordered_map&& other );
 
         //методы =============================================================================
-        unordered_map& operator=( unordered_map&& other );                                  //1
+        unordered_map& operator=( unordered_map&& other );                                  //А
         unordered_map& operator=( const unordered_map& other );
 
         //Iterators
-        template <typename T>                                                               //2
-        T *begin();
+        template <typename T>                                                               //Кс
+        iterator begin();
         template <typename T>
-        const T *begin() const;
+        const_iterator begin() const;
 
-        template <typename T>                                                               //3
-        T *end();
+        template <typename T>                                                               //Кс
+        iterator end();
         template <typename T>
-        const T *end() const;
+        const_iterator end() const;
 
         //Capacity
-        bool empty() const;                                                                 //4
+        bool empty() const;                                                                 //У
 
-        std::size_type size() const;                                                        //5
+        std::size_type size() const;                                                        //Кт
 
-        std::size_type max_size() const;                                                    //6
+        std::size_type max_size() const;                                                    //Кт
 
         //Modifiers
-        void clear();                                                                       //7
+        void clear();                                                                       //А
 
-        std::pair<iterator,bool> insert( const value_type& value );                         //8
+        std::pair<iterator,bool> insert( const value_type& value );                         //У
         std::pair<iterator,bool> insert( value_type&& value );
         template< typename P >
         std::pair<iterator,bool> insert( P&& value );
@@ -103,7 +103,7 @@ namespace cat
         insert_return_type insert( node_type&& nh );
         iterator insert( const_iterator hint, node_type&& nh );
 
-        template< typename M >                                                              //9
+        template< typename M >                                                              //У
         std::pair<iterator, bool> insert_or_assign( const Key& k, M&& obj );
         template< typename M >
         std::pair<iterator, bool> insert_or_assign( Key&& k, M&& obj );
@@ -112,13 +112,13 @@ namespace cat
         template< typename M >
         iterator insert_or_assign( const_iterator hint, Key&& k, M&& obj );
 
-        template< typename... Args >                                                        //10
+        template< typename... Args >                                                        //Кт
         std::pair<iterator,bool> emplace( Args&&... args );
 
-        template <typename... Args>                                                         //11
+        template <typename... Args>                                                         //Кт
         iterator emplace_hint( const_iterator hint, Args&&... args );
 
-        template< typename... Args >                                                        //12
+        template< typename... Args >                                                        //Кт
         std::pair<iterator, bool> try_emplace( const Key& k, Args&&... args );
         template< typename... Args >
         std::pair<iterator, bool> try_emplace( Key&& k, Args&&... args );
@@ -127,96 +127,76 @@ namespace cat
         template< typename... Args >
         iterator try_emplace( const_iterator hint, Key&& k, Args&&... args );
         
-        iterator erase( iterator pos );                                                     //13
+        iterator erase( iterator pos );                                                     //Кт
         iterator erase( const_iterator pos );
         iterator erase( const_iterator first, const_iterator last );
         size_type erase( const Key& key );
         template< typename K >
         size_type erase( K&& x );
 
-        void swap( unordered_map& other );                                                  //14
+        void swap( unordered_map& other );                                                  //Кс
 
-        node_type extract( const_iterator position );                                       //15
-        node_type extract( const Key& k );
-        template< typename K >
-        node_type extract( K&& x );
-
-        template<typename H2, typename P2>                                                  //16
+        template<typename H2, typename P2>                                                  //А
         void merge( unordered_map<Key, T, H2, P2>& source );
 
         //Lookup
-        T& at( const Key& key );                                                            //17
+        T& at( const Key& key );                                                            //У
         const T& at( const Key& key ) const;
 
-        T& operator[]( const Key& key );                                                    //18
+        T& operator[]( const Key& key );                                                    //У
         T& operator[]( Key&& key );
 
-        size_type count( const Key& key ) const;                                            //19
+        size_type count( const Key& key ) const;                                            //А
         template< typename K >
         size_type count( const K& x ) const;
 
-        iterator find( const Key& key );                                                    //20
+        iterator find( const Key& key );                                                    //У
         const_iterator find( const Key& key ) const;
         template< typename K >
         iterator find( const K& x );
         template< typename K >
         const_iterator find( const K& x ) const;
 
-        bool contains( const Key& key ) const;                                              //21
+        bool contains( const Key& key ) const;                                              //У
         template< typename K >
         bool contains( const K& x ) const;
 
-        std::pair<iterator,iterator> equal_range( const Key& key );                         //22
-        std::pair<const_iterator,const_iterator> equal_range( const Key& key ) const;
-        template< typename K >
-        std::pair<iterator,iterator> equal_range( const K& x );
-        template< typename K >
-        std::pair<const_iterator,const_iterator> equal_range( const K& x ) const;
-
         //Bucket interface
-        local_iterator begin( size_type n );                                                //23
+        local_iterator begin( size_type n );                                                //Кс
         const_local_iterator begin( size_type n ) const;
 
-        local_iterator end( size_type n );                                                  //24
+        local_iterator end( size_type n );                                                  //Кс
         const_local_iterator end( size_type n ) const;
 
-        size_type bucket_count() const;                                                     //25
+        size_type bucket_count() const;                                                     //Кт
 
-        size_type max_bucket_count() const;                                                 //26
+        size_type max_bucket_count() const;                                                 //Кт
 
-        size_type bucket_size( size_type n ) const;                                         //27
+        size_type bucket_size( size_type n ) const;                                         //Кт
 
-        size_type bucket( const Key& key ) const;                                           //28
+        size_type bucket( const Key& key ) const;                                           //Кт
 
         //Hash policy
-        float load_factor() const;                                                          //29
+        float load_factor() const;                                                          //Кс
 
-        float max_load_factor() const;                                                      //30
+        float max_load_factor() const;                                                      //Кс
         void max_load_factor( float ml );
 
-        void rehash( size_type count );                                                     //31
+        void rehash( size_type count );                                                     //А
 
-        void reserve( size_type count );                                                    //32
+        void reserve( size_type count );                                                    //А
 
         //Observers
-        hasher hash_function() const;                                                       //33
+        hasher hash_function() const;                                                       //Кс
 
-        key_equal key_eq() const;                                                           //34
+        key_equal key_eq() const;                                                           //Кс
 
         //Non-member functions
-        template <typename Key, typename T, typename Hash, typename KeyEqual>               //35
-        bool operator==( const unordered_map<Key, T, Hash, KeyEqual>& lhs,
-                         const unordered_map<Key, T, Hash, KeyEqual>& rhs );
-
-        template <typename Key, typename T, typename Hash, typename KeyEqual>               //36
-        bool operator!=( const unordered_map<Key, T, Hash, KeyEqual>& lhs,
-                         const unordered_map<Key, T, Hash, KeyEqual>& rhs );
-
-        template <typename Key, typename T, typename Hash, typename KeyEqual>               //37
+        template <typename Key, typename T, typename Hash, typename KeyEqual>               //А
         void swap( unordered_map<Key, T, Hash, KeyEqual>& lhs,
                    unordered_map<Key, T, Hash, KeyEqual>& rhs );
 
-        template <typename Key, typename T, typename Hash, typename KeyEqual, typename Pred>//38
+        template <typename Key, typename T, typename Hash, typename KeyEqual, typename Pred>//У
         typename unordered_map<Key, T, Hash, KeyEqual>::size_type
             erase_if( unordered_map<Key, T, Hash, KeyEqual>& c, Pred pred );
     
